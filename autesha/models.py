946e53d27871b51f37ebe8cdfb0fe1ff796bf034
Naggita Keziah <knaggita@gmail.com>
2016-10-15 06:48:23 +0300

    Revert "Added the Staff model"

--- autesha/models.py
+++ autesha/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.utils import timezone
 
-class Staff (models.Model):
-    name = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    bio = models.TextField()
-
-    def __str__(self):
-        return self.name
-
+# Create your models here.
